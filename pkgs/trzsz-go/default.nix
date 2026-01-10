6e2e979262a3b684f12eb273518e236198d970e5
mxmf <2993747433@qq.com>
2026-01-10 20:15:44 +0800

    feat(pkgs): add trzsz-go package

---
+++ pkgs/trzsz-go/default.nix
@@ -0,0 +1,27 @@
+{ lib, buildGoModule, fetchFromGitHub, nix-update-script }:
+
+buildGoModule rec {
+  pname = "trzsz-go";
+  version = "1.1.8";
+
+  src = fetchFromGitHub {
+    owner = "trzsz";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-g1fbgKTFS9aPAmnTeFYoymrDEoZ6BtzUhA2Z9SNYbsU=";
+  };
+
+  vendorHash = "sha256-AsrRHHBlzW5s/PtJSQ+hAgqUIYwDwoemQaerRV/QKX0=";
+
+  doCheck = false;
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = with lib; {
+    description = "trzsz-go makes all terminals that support local shell to support trzsz ( trz / tsz ), which similar to ( rz / sz ), and compatible with tmux.";
+    homepage = "https://trzsz.github.io/go";
+    license = licenses.mit;
+    maintainers = [ ];
+  };
+}
+
