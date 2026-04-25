7d0e3fe01daea559abab87c59f35001bebac4d6b
mxmf <2993747433@qq.com>
2026-04-25 21:43:29 +0800

    update texslide
    Signed-off-by: mxmf <2993747433@qq.com>

--- pkgs/texslide/default.nix
+++ pkgs/texslide/default.nix
@@ -5,12 +5,12 @@
 }:
 
 let
-  version = "0.9.3";
+  version = "0.9.4";
   pname = "texslide";
 
   src = fetchurl {
     url = "https://download.texslide.com/release/texslide-x64.AppImage";
-    hash = "sha256-98sBK0g3M9FTqS2uSCuYulwHBV+eNNceW1P5ylioEQQ=";
+    hash = "sha256-VcEuPVGwAW8SysYjkV/p5CxcFZg4crP+I+I8BdDNOac=";
   };
 
 
